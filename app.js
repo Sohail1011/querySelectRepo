9a88cf89d8f3c3983512d5cba8585e0560505a98
Sohail Iran <aratavalga@gmail.com>
2023-09-25 22:22:14 -0500

    eleccion

--- app.js
+++ app.js
@@ -3,3 +3,13 @@ container.classList.add('container')
 
 const titleElement = document.getElementById('content')
 titleElement.classList.add('primary')
+
+const tables = document.getElementById('tables')
+tables.classList.add('major')
+
+const subtitle = document.querySelector('h2')
+subtitle.textContent = 'DOM'
+
+const text = document.querySelector('p')
+text.innerHTML = `Su significado es: 'Document Object Document'. <br> Y sirve para dar al Javascript, la posibilidad de
+modificar <br> el frontend junto con los selectores de HTML.`
