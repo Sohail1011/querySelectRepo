ce601b32961efc9ef3f8dd45ef6f7b493f432c6f
Sohail Iran <aratavalga@gmail.com>
2023-09-25 21:24:18 -0500

    dom

---
+++ app.js
@@ -0,0 +1,5 @@
+const container = document.getElementById('content')
+container.classList.add('container')
+
+const titleElement = document.getElementById('content')
+titleElement.classList.add('primary')
